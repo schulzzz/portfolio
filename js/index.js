04c9a7233e4d1e042c003bdcca414b558a8ae480
schulzzz <alschulz@ucsd.edu>
2022-11-26 17:25:31 -0800

    'addedfiles'

---
+++ js/index.js
@@ -0,0 +1,57 @@
+function instagram() {
+    window.open("https://www.instagram.com/", '_blank');
+}
+
+function email() {
+    parent.location='mailto:noreply@apple.com';
+}
+
+function twitter() {
+    window.open("https://www.twitter.com/", '_blank');
+}
+
+function github() {
+    window.open("https://www.github.com/", '_blank');
+}
+
+function linkedin() {
+    window.open("https://www.linkedin.com/", '_blank');
+}
+
+function handshake() {
+    window.open("https://www.joinhandshake.com/", '_blank');
+}
+
+function youtube() {
+    window.open("https://www.youtube.com/", '_blank');
+}
+
+function resume() {
+    window.open("documents/schulz_resume_redacted.pdf",
+    "", "width=300, height=300");
+}
+
+function carouselProject() {
+    window.open("https://acsweb.ucsd.edu/~alschulz/Quiz4_Inclass_FILES/01-starter_wk4_bootstrap.html", '_blank')
+}
+
+function studyBuddy() {
+    window.open("https://acsweb.ucsd.edu/~alschulz/SchulzLab3.html", '_blank');
+}
+
+function wedding() {
+    window.open("https://acsweb.ucsd.edu/~alschulz/wedding.html", '_blank');
+}
+
+function dice() {
+    window.open("https://acsweb.ucsd.edu/~alschulz/dice/dice.html", '_blank');
+}
+
+function photoshop() {
+    window.open("images/photoshop.jpg","Photoshop Demo","width=400,height=300");
+}
+
+function schedule() {
+    window.open("https://acsweb.ucsd.edu/~alschulz/SchulzLab2.html", '_blank');
+}
+
